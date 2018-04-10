1ab5208a5dc1897533edb1e553a9e7f297a94dbd
TylerHanavan <TylerHanavan@gmail.com>
2018-04-10 12:15:13 -0400

    enemies

--- library.c
+++ library.c
@@ -1,3 +1,21 @@
+
+
+
+void init() {
+	
+	
+	
+}
+
+// ENEMY SHIPS (EXCLUDING MOTHER SHIP) \\
+// EACH ENEMY IS COMPOSED BY AN INT. THE INT (32-bit) STORES INFO
+// ABOUT EACH ENEMY, SUCH AS id (bit 0-5), alive (bit 6), 
+// type (bit 7), x location (bit 8-12), y location (bit 13-17), 
+
+
+
+// WALLS \\
+
 int is_wall_vertical(int x, int y) {
 	return x == 0 || x == 16;
 }
