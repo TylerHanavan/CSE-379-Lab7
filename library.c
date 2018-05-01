6e3082d4a7b91a0903468bf511ba43731a288b90
Christian Caballero <ccaballe@timberlake.cse.buffalo.edu>
2018-05-01 12:54:39 -0400

    collision/enemies

--- library.c
+++ library.c
@@ -21,8 +21,7 @@ int lives = 3;
 
 int score_total_;
 
-int enemies = 0xFFFFFFFF;
-int enemies_types = 0xFFFFFFFF;
+long enemies = 0xFFFFFFFF;
 int shields = 0xFFFFFFFF;
 int shields_types = 0xFFFFFFFF;
 
@@ -62,6 +61,10 @@ char* get_shield_type(int x, int y) {
 	return (shields_types & (1 << get_shield_from_coordinates(x, y))) > 0 ? "S\0" : "s\0";
 }
 
+int get_shield_type_int(int x, int y) {
+	return (shields_types & (1 << get_shield_from_coordinates(x, y))) > 0;
+}
+
 int is_shield(int x, int y) {
 	return is_shield_alive(get_shield_from_coordinates(x, y));
 }
@@ -76,12 +79,65 @@ void set_shield_alive(int loc, int alive) {
 	else shields = shields & ~(1 << loc);
 }
 
+int get_enemy_from_coordinates(int x, int y) {
+	
+	for(int j = 0; j < 5; j++) {
+		for(int i = 0; i < 7; i++) {
+			if(y == 6 - j && x == 7 + i) return i + j * 7;
+		}
+	}
+	
+	return -1;
+	
+}
+
+long get_enemy_alive(int loc){
+	if(loc == -1) return 0;
+	return (long)(enemies & (long)(1 << loc)) > 0 ? 1 : 0;
+}
+
+int is_player_bullet(int x, int y) {
+	return x == player_bullet_x && y == player_bullet_y;
+}
+
+int get_enemy_type(int loc){
+	if(loc > 27) return 2;
+	if(loc < 14) return 0;
+	else return 1;
+}
+
+char* get_enemy_char(int type){
+	if(type == 0) return "W\0";
+	else if(type == 1) return "M\0";
+	else return "O\0";
+}	
+
+
+void set_enemy_alive(int loc, int alive){
+	if(alive == 1) enemies = enemies | (1 << loc);
+	else enemies = enemies & ~(1 << loc);		
+}
 int collides() {
 	
-	if(is_shield(player_bullet_x, player_bullet_y) == 3000) {
-		set_shield_type(get_shield_from_coordinates(player_bullet_x, player_bullet_y), 0);
-		player_bullet_x = -1;
-		player_bullet_y = -1;
+	if(is_shield(player_bullet_x, player_bullet_y)) {
+		int id = get_shield_from_coordinates(player_bullet_x, player_bullet_y);
+		if(is_shield_alive(id)){
+			 if(get_shield_type_int(player_bullet_x, player_bullet_y) == 0) {
+				 set_shield_alive(id, 0);
+			 } else {
+					set_shield_type(get_shield_from_coordinates(player_bullet_x, player_bullet_y), 0);
+			 }
+			player_bullet_x = -1;
+			player_bullet_y = -1;
+		}
+		return 1;
+	}
+	
+	int id = get_enemy_from_coordinates(player_bullet_x, player_bullet_y);
+	if(id != -1 && get_enemy_alive(id) == 1) {
+			set_enemy_alive(id, 0);
+			player_bullet_x = -1;
+			player_bullet_y = -1;
 		return 1;
 	}
 	
@@ -94,9 +150,7 @@ int collides() {
 	
 }
 
-int is_player_bullet(int x, int y) {
-	return x == player_bullet_x && y == player_bullet_y;
-}
+
 
 // GAME BOARD \\
 
@@ -113,11 +167,15 @@ char* get_char_at(int x, int y) {
 	
 	if(is_player_bullet(x, y)) return "^\0";
 	
+	if(get_enemy_from_coordinates(x,y) != -1 && get_enemy_alive(get_enemy_from_coordinates(x,y)) == 1) return get_enemy_char(get_enemy_type(get_enemy_from_coordinates(x,y)));
+	
 	return " \0";
 	
 }
 
 void draw_board() {
+	
+	output_character(0xC);
 	
 	for(int j = 0; j < 15; j++) {
 		for(int i = 0; i < 21; i++) {
